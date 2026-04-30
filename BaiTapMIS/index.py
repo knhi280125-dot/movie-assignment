757deb3a4ff4f061c022c0383ebbad1f155a6a82
knhi280125-dot <knhi280125@gmail.com>
2026-04-30 20:37:35 +0800

    Update index.py

--- BaiTapMIS/index.py
+++ BaiTapMIS/index.py
@@ -1,3 +1,42 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+import certifi
+from datetime import datetime
+import os
+
+# Khởi tạo app với đường dẫn templates chính xác
+app = Flask(__name__, template_folder='templates')
+
+# MongoDB 連線資訊
+uri = "mongodb+srv://knhi280125_db_user:x3N6DpTxIzBlfzZ7@cluster0.bdra68f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri, tlsCAFile=certifi.where())
+db = client['movie_db']
+movies_col = db['movies']
+
 @app.route('/')
 def index():
-    return render_template('index.html') # Để nó hiện ra cái bảng nút bấm Nhi vừa tạo
+    return render_template('index.html')
+
+@app.route('/movie2')
+def movie2():
+    upcoming_movies = [
+        {"title": "死侍與鋼鐵人", "release_date": "2024-07-26"},
+        {"title": "小丑：雙重瘋狂", "release_date": "2024-10-04"},
+        {"title": "海洋奇緣 2", "release_date": "2024-11-27"}
+    ]
+    movies_col.delete_many({})
+    movies_col.insert_many(upcoming_movies)
+    update_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render_template('movie2.html', movies=upcoming_movies, update_time=update_time)
+
+@app.route('/movie3', methods=['GET', 'POST'])
+def movie3():
+    query_result = []
+    keyword = ""
+    if request.method == 'POST':
+        keyword = request.form.get('keyword')
+        query_result = list(movies_col.find({"title": {"$regex": keyword, "$options": "i"}}))
+    return render_template('movie3.html', movies=query_result, keyword=keyword)
+
+# Dòng này cực kỳ quan trọng để chạy trên Vercel
+app.debug = True
